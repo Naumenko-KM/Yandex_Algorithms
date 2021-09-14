2065a823bd9d90f15906408cefa7e0d577952b9d
Naumenko-KM <Naumenko-KM@yandex.ru>
2021-09-14 15:33:32 +0300

    Added 2F

--- 2F - Чемпионат по метанию коровьих лепешек.py
+++ 2F - Чемпионат по метанию коровьих лепешек.py
@@ -1,12 +1,27 @@
+n = int(input())
 numbers = list(map(int, input().split()))
 
 
-def bigger_then_neighbors(numbers):
-    count = 0
+def find_max_place(numbers):
+    vasya_place = 0
+    vasya_value = 0
     if len(numbers) > 2:
-        for i in range(len(numbers)-2):
-            if numbers[i] < numbers[i+1] and numbers[i+1] > numbers[i+2]:
-                count += 1
-    return count 
+        max_value = max(numbers)
+        max_index = numbers.index(max_value)
+        min_value = min(numbers)
+        min_index = numbers.index(min_value)
 
-print(bigger_then_neighbors(numbers))
+        for i in range(min_index-1, len(numbers)-1):
+            if max_index < i and numbers[i+1] == min_value:
+                if numbers[i]%5 == 0 and (numbers[i]/5)%2==1:
+                    if numbers[i] > vasya_value:
+                        vasya_value = numbers[i]
+
+        if vasya_value != 0:
+            vasya_place = 1
+            for value in numbers:
+                if value > vasya_value:
+                    vasya_place += 1
+    return vasya_place 
+
+print(find_max_place(numbers))
